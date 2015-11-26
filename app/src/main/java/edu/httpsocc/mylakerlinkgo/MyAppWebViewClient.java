8b20eb8fc5acd95c6243fc9acabcd412514f80bb
Chris Eckersley <c.eckersley@gmail.com>
2015-11-25 20:32:11 -0800

    Revert "Build success!  Internal URLs and external applications working!!!!  :-)"
    This reverts commit ba057285bbfb3b22706cede59bf764d91949e073.

--- app/src/main/java/edu/httpsocc/mylakerlinkgo/MyAppWebViewClient.java
+++ app/src/main/java/edu/httpsocc/mylakerlinkgo/MyAppWebViewClient.java
@@ -2,29 +2,25 @@ package edu.httpsocc.mylakerlinkgo;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import static android.support.v4.app.ActivityCompat.startActivity;
-
 /**
  * Created by 130308 on 11/25/2015.
  */
-
 public class MyAppWebViewClient extends WebViewClient {
+
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        if(url.contains("mylakerlink.socc.edu/go")) {
-            view.loadUrl(url);
-        } else {
-            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-            startActivity(i);
+        if(Uri.parse(url).getHost().endsWith("mylakerlink.socc.edu/go")) {
+            return false;
         }
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        view.getContext().startActivities(new Intent[]{intent});
         return true;
+
     }
 
-    private void startActivity(Intent i) {
-    }
 }
